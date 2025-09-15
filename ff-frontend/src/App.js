719fdc662e326e4700e2076c3f82ba8f94503b74
saitejamandadi99 <saitejamandadi99>
2025-09-15 17:50:21 +0530

    removed the routers in the app to remove the 2 router errors

--- ff-frontend/src/App.js
+++ ff-frontend/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import UserDashboard from "./pages/UserDashboard/UserDashboard";
 import UserForm from "./pages/UserForm/UserForm";
 import UserDetails from "./pages/UserDetails/UserDetails";
 
 function App() {
   return (
-    <Router>
+
       <Routes>
         <Route path="/" element={<UserDashboard />} />
         <Route path="/create" element={<UserForm />} />
         <Route path="/user/:id" element={<UserDetails />} />
       </Routes>
-    </Router>
+
   );
 }
 
