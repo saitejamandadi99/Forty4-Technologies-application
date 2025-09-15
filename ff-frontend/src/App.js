612a5916ce4eb1a9c28adf228b82a5221ac9e629
saitejamandadi99 <saitejamandadi99>
2025-09-15 17:25:33 +0530

    updated the app component for navigation

--- ff-frontend/src/App.js
+++ ff-frontend/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import UserDashboard from './pages/UserDashboard/UserDashboard.js'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserDashboard from "./pages/UserDashboard/UserDashboard";
+import UserForm from "./pages/UserForm/UserForm";
+import UserDetail from "./pages/UserDetail/UserDetail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Forty4 Technologies</h1>
-      <UserDashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserDashboard />} />
+        <Route path="/create" element={<UserForm />} />
+        <Route path="/user/:id" element={<UserDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
